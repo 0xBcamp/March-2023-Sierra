662452325b493e2797d69dadab477096b070901d
fesimaxu <fesimaxu30@gmail.com>
2023-04-01 16:36:00 +0100

    update

--- packages/hardhat/contracts/MultiSigVault.sol
+++ packages/hardhat/contracts/MultiSigVault.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./interface/Uniswap.sol";
+import "./interface/IUniswap.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract multiSigVault {
